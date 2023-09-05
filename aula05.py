849d62495436dfbfdedfad4d6e64e16ec2a081d5
Priscila Portela <prisciladayelleggg@hotmail.com>
2023-09-05 22:34:20 +0100

    atividade idade minima

---
+++ aula05.py
@@ -0,0 +1,8 @@
+
+idade = int(input("Digite sua idade: "))
+
+# Verifica se a idade é maior ou igual a 18
+if idade >= 18:
+    print("Possui idade para dirigir.")
+else:
+    print("Não possui idade para dirigir.")
